b57e08ab890663deb497c01c9c00927a7a584cc8
devnonestop <nonestopdev.gmail..com>
2026-03-22 00:39:49 +0200

    Initial commit: Expo app with NativeWind and Metro web setup

---
+++ UberCloneApp/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text>Hello World</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
